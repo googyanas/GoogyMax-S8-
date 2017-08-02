e2b8d5ccba47929cd9db8fe15a81b5600b3748f9
googyanas <anas.reggoug@gmail.com>
2017-08-02 13:32:07 +0100

    4.4.22

--- drivers/usb/host/xhci-ring.c
+++ drivers/usb/host/xhci-ring.c
@@ -846,6 +846,10 @@ remove_finished_td:
 		 */
 		xhci_giveback_urb_in_irq(xhci, cur_td, 0);
 
+	if (xhci->xhc_state & XHCI_STATE_REMOVING) {
+		spin_unlock_irqrestore(&xhci->lock, flags);
+		return;
+	}
 		/* Stop processing the cancelled list if the watchdog timer is
 		 * running.
 		 */
@@ -994,7 +998,7 @@ void xhci_stop_endpoint_command_watchdog(unsigned long arg)
 	spin_unlock_irqrestore(&xhci->lock, flags);
 	xhci_dbg_trace(xhci, trace_xhci_dbg_cancel_urb,
 			"Calling usb_hc_died()");
-	usb_hc_died(xhci_to_hcd(xhci)->primary_hcd);
+	usb_hc_died(xhci_to_hcd(xhci));
 	xhci_dbg_trace(xhci, trace_xhci_dbg_cancel_urb,
 			"xHCI host controller is dead.");
 }
